b646a5044d9f7336c018f84a668819aa5d31743d
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-18 23:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
-
+function toggleTheme() {
+	const maintheme = document.getElementById('app');
+	const btntheme = document.getElementById('swap');
+	maintheme.classList.toggle('night');
+	btntheme.classList.toggle('button_night');
+	// document.getElementById('app').className='night';
+	// document.getElementById('swap').className='button_night';
+}
